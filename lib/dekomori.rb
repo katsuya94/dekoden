31653624a41bc56128388427c0e6ff1e609d1baa
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2016-10-29 14:34:46 -0500

    First version

--- lib/dekomori.rb
+++ lib/dekomori.rb
@@ -1,5 +1,95 @@
 require "dekomori/version"
 
 module Dekomori
-  # Your code goes here...
+  class Decorator
+    def initialize(*args, &blk)
+    end
+
+
+    def around
+      yield
+    end
+  end
+
+  class Collection < Module
+    def initialize
+      @clients = {}
+      @decorator_methods = Module.new
+      kollection_clients = @clients
+      kollection_decorator_methods = @decorator_methods
+
+      base_decorator_klass = Class.new(Decorator)
+
+      base_decorator_klass.define_singleton_method(:inherited) do |decorator_klass|
+        kollection_decorator_methods.module_eval do
+          define_method(decorator_klass.to_s.to_sym) do |*args, &blk|
+            decorator = decorator_klass.new(*args, &blk)
+            kollection_clients[self] << decorator
+            decorator
+          end
+        end
+      end
+
+      const_set(:Decorator, base_decorator_klass)
+    end
+
+    def included(base_klass)
+      @clients[base_klass] = []
+      kollection_clients = @clients
+
+      base_klass.extend(@decorator_methods)
+
+      decorated_instance_methods = Module.new
+      base_klass.const_set(:DecoratedMethods, decorated_instance_methods)
+      base_klass.prepend(decorated_instance_methods)
+
+      decorated_singleton_methods = Module.new
+      base_klass.const_set(:DecoratedSingletonMethods, decorated_singleton_methods)
+      base_klass.singleton_class.prepend(decorated_singleton_methods)
+
+      base_klass.define_singleton_method(:method_added) do |method_name|
+        method = self.instance_method(method_name)
+        unless kollection_clients[self].empty?
+          decorators = kollection_clients[self].dup
+          kollection_clients[self].clear
+          decorated_instance_methods.module_eval do
+            define_method(method.name) do |*args, &blk|
+              Helpers.wrap(decorators) do
+                method.bind(self).call(*args, &blk)
+              end
+            end
+          end
+        end
+        super(method_name)
+      end
+
+      base_klass.define_singleton_method(:singleton_method_added) do |method_name|
+        method = self.method(method_name)
+        unless kollection_clients[self].empty?
+          decorators = kollection_clients[self].dup
+          kollection_clients[self].clear
+          decorated_singleton_methods.module_eval do
+            define_method(method.name) do |*args, &blk|
+              Helpers.wrap(decorators) do
+                method.call(*args, &blk)
+              end
+            end
+          end
+        end
+        super(method_name)
+      end
+    end
+  end
+
+  module Helpers
+    def self.wrap(decorators, &blk)
+      if decorator = decorators.first
+        decorator.around do
+          wrap(decorators.drop(1), &blk)
+        end
+      else
+        blk.call()
+      end
+    end
+  end
 end
--- spec/dekomori_spec.rb
+++ spec/dekomori_spec.rb
@@ -5,7 +5,155 @@ describe Dekomori do
     expect(Dekomori::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  TestCollection = Dekomori::Collection.new
+
+  class Bar < TestCollection::Decorator
+    def initialize(pre, post)
+      @pre = pre
+      @post = post
+    end
+
+    def around
+      [@pre, yield, @post]
+    end
+  end
+
+  before(:each) do
+    stub_const("Foo", Class.new)
+    class Foo
+      include TestCollection
+    end
+    stub_const("Baz", Module.new)
+    module Baz
+      include TestCollection
+    end
+    stub_const("Qux", Class.new)
+    class Qux
+      include Baz
+    end
+  end
+
+  it "wraps instance methods" do
+    class Foo
+      Bar "bleep", "bloop"
+      def bap
+        "blop"
+      end
+    end
+    expect(Foo.new.bap).to eq(["bleep", "blop", "bloop"])
+  end
+
+  it "allows wrapped instance methods to take arguments" do
+    class Foo
+      Bar "cat", "mouse"
+      def bop(arg)
+        arg
+      end
+    end
+    expect(Foo.new.bop("dog")).to eq(["cat", "dog", "mouse"])
+  end
+
+  it "allows instance method decorators to be nested" do
+    class Foo
+      Bar "north", "south"
+      Bar "east", "west"
+      def bip
+        "cardinal"
+      end
+    end
+    expect(Foo.new.bip).to eq(["north", ["east", "cardinal", "west"], "south"])
+  end
+
+  it "wraps class methods" do
+    class Foo
+      Bar "meow", "woof"
+      def self.boo
+        "moo"
+      end
+    end
+    expect(Foo.boo).to eq(["meow", "moo", "woof"])
+  end
+
+  it "allows wrapped class methods to take arguments" do
+    class Foo
+      Bar "meow", "woof"
+      def self.boz(arg)
+        arg
+      end
+    end
+    expect(Foo.boz("goo")).to eq(["meow", "goo", "woof"])
+  end
+
+  it "allows class method decorators to be nested" do
+    class Foo
+      Bar "north", "south"
+      Bar "east", "west"
+      def self.bip
+        "cardinal"
+      end
+    end
+    expect(Foo.bip).to eq(["north", ["east", "cardinal", "west"], "south"])
+  end
+
+  it "wraps module methods" do
+    module Baz
+      Bar "meow", "woof"
+      def self.boo
+        "moo"
+      end
+    end
+    expect(Baz.boo).to eq(["meow", "moo", "woof"])
+  end
+
+  it "allows wrapped module methods to take arguments" do
+    module Baz
+      Bar "meow", "woof"
+      def self.boz(arg)
+        arg
+      end
+    end
+    expect(Baz.boz("goo")).to eq(["meow", "goo", "woof"])
+  end
+
+  it "allows module method decorators to be nested" do
+    module Baz
+      Bar "north", "south"
+      Bar "east", "west"
+      def self.bip
+        "cardinal"
+      end
+    end
+    expect(Baz.bip).to eq(["north", ["east", "cardinal", "west"], "south"])
+  end
+
+  it "wraps module instance methods" do
+    module Baz
+      Bar "meow", "woof"
+      def boo
+        "moo"
+      end
+    end
+    expect(Qux.new.boo).to eq(["meow", "moo", "woof"])
+  end
+
+  it "allows wrapped module instance methods to take arguments" do
+    module Baz
+      Bar "meow", "woof"
+      def boz(arg)
+        arg
+      end
+    end
+    expect(Qux.new.boz("goo")).to eq(["meow", "goo", "woof"])
+  end
+
+  it "allows module instance method decorators to be nested" do
+    module Baz
+      Bar "north", "south"
+      Bar "east", "west"
+      def bip
+        "cardinal"
+      end
+    end
+    expect(Qux.new.bip).to eq(["north", ["east", "cardinal", "west"], "south"])
   end
 end
